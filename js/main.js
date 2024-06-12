b01cba003c7a2c4e5b0b67f7fa0a080669566e45
topetusam <67940360+topetusam@users.noreply.github.com>
2024-06-12 08:57:14 -0500

    feat: :sparkles: adding a adding javasrpit and css

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let header__information = document.querySelector('.header__information');
+console.dir(header__information);
+
+let input_search = document.querySelector("#input_search");
+
+input_search.addEventListener("", );
